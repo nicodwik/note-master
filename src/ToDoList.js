fd58cfb367476899698b2318f5164d4ba6f2c154
Nico Dwi Kuswanto <nicodwika@gmail.com>
2020-07-11 17:45:53 +0700

    removed checkbox

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -9,7 +9,7 @@ const ToDoList = ({todos, deleteTodo}) => {
         <List>
             {todos.map((todo, index) => (
                 <ListItem key={index.toString()} dense button>
-                    <Checkbox tabIndex={-1} disableRipple />
+                    
                     <ListItemText primary={todo} />
                     <ListItemSecondaryAction>
                         <IconButton aria-label='Delete' onClick={() => {deleteTodo(index)}}>
